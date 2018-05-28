114b04ebfc4f663da7437751f85023b3c509304e
badf00d21 <jiricekova31@gmail.com>
2018-05-28 13:14:21 +0200

    prepis inventara bgb

--- src/main/java/bisis/export/MigrateBGB.java
+++ src/main/java/bisis/export/MigrateBGB.java
@@ -5,27 +5,36 @@ import bisis.records.Primerak;
 import bisis.records.Record;
 import bisis.textsrv.DBStorage;
 import bisis.utils.CSVUtils;
+import bisis.utils.FileUtils;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class MigrateBGB {
 
     public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Unesite naziv mysql baze kao argument!");
+            return;
+        }
+
+        String dbName = args[0];
+
+
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bisis_bgb_04?useSSL=false&serverTimezone=CET"
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false&serverTimezone=CET"
                     , "bisis", "bisis");
             DB db = new MongoClient().getDB("bisis");
+            PrintWriter outPrimerci = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("primerciMap.json"), "UTF8")));
+            PrintWriter outGodine = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("godineMap.json"), "UTF8")));
+
 
             Jongo jongo = new Jongo(db);
             MongoCollection centralRecs = jongo.getCollection("bgb_records");
@@ -35,13 +44,21 @@ public class MigrateBGB {
 
             //listamo linije record_map.csv
             int i = 0;
+            List<String> errIDS = new ArrayList<>();
             while (scanner.hasNext()) {
                 List<String> line = CSVUtils.parseLine(scanner.nextLine());
                 Integer localId = Integer.valueOf(line.get(1));
-                String centralId = line.get(2);
+                Integer centralId = Integer.valueOf(line.get(2));
 
                 //pokupi zapis iz opstinske
                 Record localRec = storage.get(conn, localId);
+
+                if(localRec == null) {
+
+                    System.out.println("Greska, ne postoji lokalni zapis sa brojem: " + localId);
+                    errIDS.add(String.valueOf(localId));
+                }
+
                 //pokupi zapis iz centralne
                 Record centralRec = centralRecs.findOne("{rn:" + centralId + "}").as(Record.class);
 
@@ -64,6 +81,13 @@ public class MigrateBGB {
                         System.out.println("Processed: " + i + " records");
                 }
             }
+
+            FileUtils.writeTextFile("not_found_records.txt",errIDS.toString());
+//            outPrimerci.println(JSONSerializer.toJSONPrimerciMap(mapPrimerci));
+//            outGodine.println(JSONSerializer.toJSONGodineMap(mapGodine));
+//
+//            outGodine.close();
+//            outPrimerci.close();
             scanner.close();
 
 
@@ -71,7 +95,11 @@ public class MigrateBGB {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
+
+
 
     }
 
