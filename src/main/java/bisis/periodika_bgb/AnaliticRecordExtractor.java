2899f74ab9be6de3b226f618b9a8f7bfe66fca39
badf00d21 <jiricekova31@gmail.com>
2018-12-03 15:03:19 +0100

    prepis analitike bgb - in progress

--- src/main/java/bisis/periodika_bgb/AnaliticRecordExtractor.java
+++ src/main/java/bisis/periodika_bgb/AnaliticRecordExtractor.java
@@ -1,6 +1,8 @@
 package bisis.periodika_bgb;
 
+import bisis.coders.Counter;
 import bisis.jongo_records.JoRecord;
+import bisis.records.Author;
 import bisis.records.Field;
 import bisis.records.Subfield;
 import com.mongodb.MongoClient;
@@ -9,48 +11,114 @@ import org.jongo.MongoCollection;
 import org.jongo.MongoCursor;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class AnaliticRecordExtractor {
 
-    public static final String ANALITIC_CRITERIA = "{'fields': {$elemMatch: {'name': '001', 'subfields': {$elemMatch: {'name': 'c', 'content':'s'}}}}}";
+    public static final String SERIAL_PUB_CRITERIA = "{'fields': {$elemMatch: {'name': '001', 'subfields': {$elemMatch: {'name': 'c', 'content':'s'}}}}}";
     public static final int ANALITIC_TYPE = 3;
 
     public static void main(String[] args) {
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         Jongo jongo = new Jongo(mongoClient.getDB("bisis"));
         MongoCollection recordsMongoCollection = jongo.getCollection("bgb_records");
+        MongoCollection codersCountersMongoCollection = jongo.getCollection("coders.counters");
 
-        MongoCursor<JoRecord> analiticRecords = recordsMongoCollection.find(ANALITIC_CRITERIA).as(JoRecord.class);
+        MongoCursor<JoRecord> analiticRecords = recordsMongoCollection.find(SERIAL_PUB_CRITERIA).as(JoRecord.class);
+        Counter counterRn = codersCountersMongoCollection.findOne("{'library':'bgb', 'counterName':'RN'}").as(Counter.class);
+        Counter counterRecordid = codersCountersMongoCollection.findOne("{'library':'bgb', 'counterName':'recordid'}").as(Counter.class);
+        int rnCnt = counterRn.getCounterValue() + 1;
+        int recIdCnt= counterRecordid.getCounterValue() + 1;
+
         int cnt = 0;
         while (analiticRecords.hasNext()) {
             JoRecord aRec = analiticRecords.next();
             if (aRec.getField("423") != null) {
-                List<JoRecord> analitics = extractRec2Analitics(aRec);
+                List<JoRecord> analitics = extractRec2Analitics(aRec, rnCnt, recIdCnt);
+                rnCnt++;
+                recIdCnt++;
                 System.out.println(analitics);
                 cnt++;
             }
         }
         System.out.println(analiticRecords.count());
         System.out.println(cnt);
+        counterRn.setCounterValue(rnCnt);
+        counterRecordid.setCounterValue(recIdCnt);
+//        codersCountersMongoCollection.update("{counterName: 'RN', library: 'bgb'}").with(counterRn);
+//        codersCountersMongoCollection.update("{counterName: 'recordid', library: 'bgb'}").with(counterRecordid);
+        System.out.println("\nCOUNTERS FOR RN & RECORD_ID SET TO: " + rnCnt + " & " + recIdCnt);
     }
 
-    private static List<JoRecord> extractRec2Analitics(JoRecord record) {
+    private static List<JoRecord> extractRec2Analitics(JoRecord record, int rn, int record_id) {
         List<JoRecord> retVal = new ArrayList<>();
 
         for(Field f423: record.getFields("423")){
             JoRecord r = new JoRecord();
             r.setPubType(ANALITIC_TYPE);
+            r.setRecordID(record_id);
+            r.setRN(rn);
+            r.setCreator(new Author("prepis 2018", "bgb"));
+            r.setCreationDate(new Date());
 
             List<Field> allFields = new ArrayList<>();
-            allFields.addAll(getGenericAnaliticFields());
+            allFields.addAll(get001Generics(rn));
+            if (format011(record) != null) {
+                allFields.add(format011(record));
+            }
+
+            Field f100 = new Field("100");
+            f100.getSubfields().add(record.getSubfield("100c"));
+            allFields.add(f100);
+
+            if (record.getSubfieldContent("101c") != null) {
+                Field f101 = new Field("101");
+                f101.add(new Subfield('c', record.getSubfieldContent("101c")));
+                allFields.add(f101);
+                Field f210 = new Field("210");
+                Subfield sf210d = new Subfield('d', record.getSubfieldContent("100c"));
+                f210.add(sf210d);
+                allFields.add(f210);
+            }
+
+
+            Field f215 = new Field("215");
+            if (record.getSubfieldContent("200h") != null) {
+                Subfield sf215h = new Subfield('h', record.getSubfieldContent("200h"));
+                f215.add(sf215h);
+            }
+            if (record.getSubfieldContent("200i") != null) {
+                Subfield sf215k = new Subfield('k', record.getSubfieldContent("200i"));
+                f215.add(sf215k);
+            }
+            if (f215.getSubfield('i') != null || f215.getSubfield('h') != null)
+                allFields.add(f215);
+
             for (Subfield sf: f423.getSubfields()) {
                 if (sf.getSecField() != null) {
-                    allFields.add(sf.getSecField());
+                    boolean containsSf = false;
+                    for (Field field: allFields) {
+                        if (field.getName().equals(sf.getSecField().getName())){
+                            containsSf = true;
+                            for (Subfield _sf: sf.getSecField().getSubfields()) {
+                                if (field.getSubfield(sf.getName()) == null)
+                                    field.add(_sf);
+                            }
+                            break;
+                        }
+                    }
+                    if (!containsSf)
+                        allFields.add(sf.getSecField());
                 }
             }
+
+            Field f992 = new Field("992");
+            f992.add(new Subfield('b', "prepis 2018"));
+            allFields.add(f992);
+
             r.setFields(allFields);
-            r.pack();
+            //r.pack();
             retVal.add(r);
 
         }
@@ -59,7 +127,26 @@ public class AnaliticRecordExtractor {
         return retVal;
     }
 
-    private static List<Field> getGenericAnaliticFields() {
+    private static Field format011(JoRecord record) {
+        Field f011 = new Field("011");
+        String _011Content = "";
+
+        if (record.getSubfieldContent("011a") == null)
+            return null;
+
+        f011.add(new Subfield('a', record.getSubfieldContent("011a")));
+        _011Content = record.getSubfieldContent("011a");
+        if (record.getSubfieldContent("200a") != null)
+            _011Content += "(" + record.getSubfieldContent("200a");
+        if (record.getSubfieldContent("200e") != null)
+            _011Content += ":" + record.getSubfieldContent("200e");
+        _011Content += ")";
+        f011.add(new Subfield('b', _011Content));
+
+        return f011;
+    }
+
+    private static List<Field> get001Generics(int rn) {
         List<Field> retVal = new ArrayList<>();
 
         Field f001 = new Field("001");
@@ -67,6 +154,7 @@ public class AnaliticRecordExtractor {
         f001.add(new Subfield('b', "a"));
         f001.add(new Subfield('c', "a"));
         f001.add(new Subfield('d', "2"));
+        f001.add(new Subfield('e', Integer.toString(rn)));
         retVal.add(f001);
 
         return retVal;
