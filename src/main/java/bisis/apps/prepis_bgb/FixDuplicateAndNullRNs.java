5b227b1348611d718fc6d5339aebf24bd10908de
badf00d21 <petar.makevic@ftninformatika.com>
2020-08-18 17:57:06 +0200

    Popravak neispravnih RN-ova u zapisima (null ili duplirane vrednosti)

--- src/main/java/bisis/apps/prepis_bgb/FixDuplicateAndNullRNs.java
+++ src/main/java/bisis/apps/prepis_bgb/FixDuplicateAndNullRNs.java
@@ -9,22 +9,38 @@ import org.bson.types.ObjectId;
 import org.jongo.Aggregate;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
+import org.jongo.MongoCursor;
 
-public class FixDuplicateRNs {
+public class FixDuplicateAndNullRNs {
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Unesite prefiks biblioteke kao parametar!");
+        }
+        String libPref = args[0];
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         DB db = mongoClient.getDB("bisis");
         Jongo jongo = new Jongo(db);
-        MongoCollection recordsCollection = jongo.getCollection("bgb_records");
+        MongoCollection recordsCollection = jongo.getCollection(libPref + "_records");
         MongoCollection counterCoderCollection = jongo.getCollection("coders.counters");
+        Counter rnCounter = counterCoderCollection.findOne("{ library:'"+ libPref +"', counterName: 'RN' }").as(Counter.class);
+        int nextRn = rnCounter.getCounterValue();
 
+        //-----------NULLs-------------------
+        MongoCursor<JoRecord> recsWithNull = recordsCollection.find("{rn: null}").as(JoRecord.class);
+        System.out.println("Started changing records with NULL RN values...");
+        while (recsWithNull.hasNext()) {
+            JoRecord record = recsWithNull.next();
+            nextRn++;
+            record.setRN(nextRn);
+            recordsCollection.save(record);
+        }
+
+        //-----------DUPLICATES--------------
         Aggregate.ResultsIterator<JoDupRec> dupRecsIterator = recordsCollection.aggregate("{$group: {_id:'$rn', dups: { $addToSet: '$_id'}, count: {$sum:1}}}}")
                          .and("{$match: { count: {'$gt':1}}}").as(JoDupRec.class);
 
-        Counter rnCounter = counterCoderCollection.findOne("{ library:'bgb', counterName: 'RN' }").as(Counter.class);
-        int nextRn = rnCounter.getCounterValue();
-        System.out.println("Started processing records...");
+        System.out.println("Started processing records with duplicate RNs...");
         while(dupRecsIterator.hasNext()) {
             JoDupRec dupRec = dupRecsIterator.next();
             for(ObjectId _id: dupRec.getDups()) {
@@ -39,4 +55,5 @@ public class FixDuplicateRNs {
         counterCoderCollection.save(rnCounter);
         System.out.println("Done!");
     }
+
 }
